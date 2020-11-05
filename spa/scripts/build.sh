3635c972a57e24b14078122e3653901782a58494
Bartosz Staryga <bartosz.staryga@magnolia-cms.com>
2020-11-05 16:37:38 +0100

    Update build script

--- spa/scripts/build.sh
+++ spa/scripts/build.sh
@@ -2,4 +2,4 @@
 export PUBLIC_URL="/magnoliaAuthor/.resources/spa-lm/webresources/build"
 
 npm run build
-cp -r ../build ../../light-modules/spa-lm/webresources/
+rsync -a --delete ../build/ ../../light-modules/spa-lm/webresources/build/
